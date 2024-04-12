a5fb41dc2543e4f197981b5ffa252c06e902ae83
TrashPanda012 <kalbrowne@uamv.edu.ni>
2024-04-12 07:31:34 -0600

    ejercicio numPares

---
+++ ejemplo1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hola \n");
+    printf("mi nombre es: Kevin");
+    return 0;
+}
---
+++ numerosPares.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/*Almacenar 5 numeros pares*/
+int main(int argc, char const *argv[])
+{
+    int numbers[5];
+    numbers [0] = 2;
+    numbers [1] = 44;
+    numbers [2] = 106;
+    numbers [3] = 1000;
+    numbers [4] = 2024;
+    
+    printf("Elemento #1: %d", numbers[0]);
+    printf("Elemento #2: %d", numbers[1]);
+    printf("Elemento #3: %d", numbers[2]);
+    printf("Elemento #4: %d", numbers[3]);
+    printf("Elemento #5: %d", numbers[4]);
+    return 0;
+}
