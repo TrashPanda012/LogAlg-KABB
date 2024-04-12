bb84b331319aad2680202b6122e40a1880e98db4
TrashPanda012 <kalbrowne@uamv.edu.ni>
2024-04-12 08:02:17 -0600

    arreglo caracter

---
+++ arregloCaracter.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/*Arreglos de las vocales*/
+
+int main(int argc, char const *argv[])
+{
+    char vocals[6];
+
+    vocals[0] = 'a';
+    vocals[1] = 'e';
+    vocals[2] = 'i';
+    vocals[3] = 'o';
+    vocals[4] = 'u';
+
+
+    printf("\n Elemento #1: %s", vocals);
+    printf("\n Elemento #1: %c", vocals[0]);
+    printf("\n Elemento #2: %c", vocals[1]);
+    printf("\n Elemento #3: %c", vocals[2]);
+    printf("\n Elemento #4: %c", vocals[3]);
+    printf("\n Elemento #5: %c", vocals[4]);
+
+    return 0;
+}
