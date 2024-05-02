54fbf47344683f93c103506f808e3f16e51183e6
TrashPanda012 <kalbrowne@uamv.edu.ni>
2024-05-02 12:11:58 -0600

    primer ejercicio practico

--- ejercicios prácticos #6/ejercicio1.cpp
+++ ejercicios prácticos #6/ejercicio1.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n;
-    int suma = 0;
+void imprimirNum(int n);
+int n;
+int suma = 0;
 
+int main()
+{
     cout << "Ingrese un numero natural n: ";
     cin >> n;
+    imprimirNum(n);
+    return 0;
+}
+
+void imprimirNum(int n){
     for (int i = 1; i <= n; ++i) {
         suma += i;
     }
     cout << "La suma de los numeros naturales desde 1 hasta " << n << " es: " << suma << endl;
-
-    return 0;
 }
