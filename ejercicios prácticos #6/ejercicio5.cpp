b98aa35e0f50e47c079ecf30d81ec783db01e265
TrashPanda012 <kalbrowne@uamv.edu.ni>
2024-05-02 12:58:28 -0600

    quinto ejercicio practico

---
+++ ejercicios prácticos #6/ejercicio5.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+#define numEstudiantes 8
+float notas[numEstudiantes];
+float suma = 0;
+float promedio;
+int aprobados = 0;
+int reprobados = 0;
+void pedirNotas();
+
+int main(int argc, char const *argv[])
+{
+    pedirNotas();
+    promedio = suma / numEstudiantes;
+    cout << "Cantidad de alumnos aprobados: " << aprobados << endl;
+    cout << "Cantidad de alumnos reprobados: " << reprobados << endl;
+    cout << "Promedio General del grupo: " << promedio << endl;
+    return 0;
+}
+
+void pedirNotas(){
+    for (int i = 0; i < numEstudiantes; i++)
+    {
+        cout << "Ingrese la nota del estudiante " << (i + 1) << ":";
+        cin >> notas[i];
+        suma += notas[i];
+
+        if (notas[i] >= 60)
+        {
+            aprobados++;
+        } else{
+            reprobados++;
+        }
+    }
+}
