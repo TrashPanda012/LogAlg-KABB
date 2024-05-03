47af757f62134c1cc4db51ffddeeb2fa3a92b1be
TrashPanda012 <kalbrowne@uamv.edu.ni>
2024-05-02 20:12:10 -0600

    cuarto ejercicio practico

--- ejercicios prácticos #6/ejercicio4.cpp
+++ ejercicios prácticos #6/ejercicio4.cpp
@@ -1,35 +1,27 @@
 #include <iostream>
 using namespace std;
 
-#define numEstudiantes 10;
-float nota1, nota2, nota3, nota4, nota5, nota6, nota7, nota8, nota9, nota10, suma, promedio;
+#define estudiantes 10
+float notas[estudiantes];
+float sumaNotas = 0;
+float promedio;
+void ingresarNotas();
 
 int main()
 {
-    cout << "\n Ingrese la nota del primer estudiante: "; 
-    cin >> nota1;
-    cout << "\n Ingrese la nota del segundo estudiante: "; 
-    cin >> nota2;
-    cout << "\n Ingrese la nota del tercer estudiante: "; 
-    cin >> nota3;
-    cout << "\n Ingrese la nota del cuarto estudiante: "; 
-    cin >> nota4;
-    cout << "\n Ingrese la nota del quinto estudiante: "; 
-    cin >> nota5;
-    cout << "\n Ingrese la nota del sexto estudiante: "; 
-    cin >> nota6;
-    cout << "\n Ingrese la nota del septimo estudiante: "; 
-    cin >> nota7;
-    cout << "\n Ingrese la nota del octavo estudiante: "; 
-    cin >> nota8;
-    cout << "\n Ingrese la nota del noveno estudiante: "; 
-    cin >> nota9;
-    cout << "\n Ingrese la nota del decimo estudiante: "; 
-    cin >> nota10;
-
-    suma = nota1 + nota2 + nota3 + nota4 + nota5 + nota6 + nota7 + nota8 + nota9 + nota10;
-    promedio = suma / numEstudiantes;
-
-    cout << "\n El promedio general de la seccion es igual a: " << promedio << endl;
+    cout << "Ingrese las notas de los " << estudiantes << " estudiantes: " << endl;
+    ingresarNotas();
+    promedio = sumaNotas / estudiantes;
+    cout << "El promedio de las notas es: " << promedio << endl;
     return 0;
+}
+
+void ingresarNotas(){
+    for (int i = 0; i < estudiantes; i++)
+    {
+        cout << "Estudiante " << i + 1 << ": ";
+        cin >> notas[i];
+        sumaNotas += notas[i];
+    }
+    
 }
